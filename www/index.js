96a57543d0a8e4300bf6a90a10e04372f61187d5
Matt <mr.chenyuqing@live.com>
2019-03-10 22:41:52 +0800

    add more tests and apply to js

--- www/index.js
+++ www/index.js
@@ -1,3 +1,18 @@
-import * as wasm from "../pkg";
+import { Universe } from "../pkg";
 
-wasm.greet();
+const pre = document.getElementById("game-of-life-canvas");
+const universe = Universe.new();
+
+setInterval(() => {
+    pre.textContent = universe.render();
+    universe.tick();
+}, 200);
+
+// const renderLoop = () => {
+//     pre.textContent = universe.render();
+//     universe.tick();
+
+//     requestAnimationFrame(renderLoop);
+// }
+
+// requestAnimationFrame(renderLoop);
